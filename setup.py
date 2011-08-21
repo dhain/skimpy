82e128211a35a659d2afd97c9a7f2d12484d5b3c
David Zuwenden <dhain@zognot.org>
2011-08-21 10:18:23 -0700

    initial commit

---
+++ setup.py
@@ -0,0 +1,27 @@
+import os
+from setuptools import setup, find_packages
+
+version = '0.0.1'
+README = os.path.join(os.path.dirname(__file__), 'README.rst')
+long_description = open(README).read() + '\n\n'
+
+if __name__ == '__main__':
+    setup(
+        name='skimpy',
+        version=version,
+        description=('A data conversion and validation library.'),
+        long_description=long_description,
+        author='David Zuwenden',
+        author_email='dhain@zognot.org',
+        url='https://github.com/dhain/skimpy',
+        license='MIT',
+        classifiers=[
+            "Intended Audience :: Developers",
+            "License :: OSI Approved :: MIT License",
+            "Programming Language :: Python",
+            "Topic :: Software Development :: Libraries :: Python Modules",
+        ],
+        packages=find_packages(),
+        test_suite='skimpy.test',
+        tests_require=['mock'],
+    )
