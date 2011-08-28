49d12303989937e0e2fedd15af711928ab338e5a
David Zuwenden <dhain@zognot.org>
2011-08-28 08:04:17 -0700

    start Element

---
+++ skimpy/element.py
@@ -0,0 +1,61 @@
+class ElementType(type):
+    def __init__(self, name, bases, dct):
+        self.children = set()
+        for base in bases:
+            if isinstance(base, ElementType):
+                self.children |= base.children
+        for key, value in dct.iteritems():
+            if not isinstance(value, ElementType):
+                continue
+            self.children.add(key)
+
+    def __get__(self, obj, cls):
+        attr = '_%s__parent' % (self.__name__,)
+        bound = self._mk_subclass()
+        setattr(bound, attr, cls)
+        if obj is not None:
+            bound = bound()
+            setattr(bound, attr, obj)
+        return bound
+
+    def _mk_subclass(self):
+        return type(self.__name__, (self,), {})
+
+
+class Element(object):
+    __metaclass__ = ElementType
+
+    class parent(object):
+        def __get__(self, obj, cls):
+            attr = '_%s__parent' % (cls.__name__,)
+            try:
+                if obj is None:
+                    return cls.__dict__[attr]
+                else:
+                    return obj.__dict__.get(attr, cls.__dict__[attr])
+            except (KeyError, AttributeError):
+                raise AttributeError(
+                    "%r object has no attribute 'parent'" % (cls.__name__,))
+    parent = parent()
+
+    class name(object):
+        def __get__(self, obj, cls):
+            try:
+                return obj.__dict__['name']
+            except (KeyError, AttributeError):
+                return cls.__name__
+    name = name()
+
+    class path(object):
+        def __get__(self, obj, cls):
+            path = []
+            el = cls
+            while True:
+                try:
+                    path.append(el.name)
+                    el = el.parent
+                except AttributeError:
+                    break
+            path.reverse()
+            return '.'.join(path)
+    path = path()
---
+++ skimpy/test/test_element.py
@@ -0,0 +1,107 @@
+import unittest
+
+from skimpy.element import *
+
+
+class TestElement(unittest.TestCase):
+    def test_get_returns_copy_bound_to_parent(self):
+        class A(object):
+            element = Element
+        self.assertIsNot(A.element, Element)
+        self.assertIs(A.element.parent, A)
+
+    def test_get_on_instance_returns_instance_bound_to_parent_instance(self):
+        class A(object):
+            element = Element
+        a = A()
+        self.assertTrue(isinstance(a.element, Element))
+        self.assertIs(a.element.parent, a)
+
+    def test_get_works_with_element_subclass(self):
+        class MyElement(Element):
+            pass
+        class A(object):
+            element = MyElement
+        self.assertIsNot(A.element, MyElement)
+        self.assertIs(A.element.parent, A)
+
+    def test_get_works_with_parent_subclass(self):
+        class MyElement(Element):
+            pass
+        class A(object):
+            element = MyElement
+        class B(A):
+            pass
+        self.assertIsNot(B.element, MyElement)
+        self.assertIs(B.element.parent, B)
+
+    def test_default_name_is_class_name(self):
+        class MyElement(Element):
+            pass
+        self.assertEqual(MyElement.name, 'MyElement')
+
+    def test_can_set_name_on_class(self):
+        class MyElement(Element):
+            name = 'a_name'
+        self.assertEqual(MyElement.name, 'a_name')
+
+    def test_can_set_name_on_instance(self):
+        class MyElement(Element):
+            pass
+        el = MyElement()
+        el.name = 'a_name'
+        self.assertEqual(el.name, 'a_name')
+
+    def test_path(self):
+        class E1(Element):
+            class E2(Element):
+                class E3(Element):
+                    pass
+        e = E1.E2
+        self.assertEqual(E1.E2.E3.path, 'E1.E2.E3')
+
+    def test_path_works_when_supplanted(self):
+        class E1(Element):
+            class E2(Element):
+                class E3(Element):
+                    pass
+        class E4(Element):
+            pass
+        E4.E3 = E1.E2.E3
+        self.assertEqual(E4.E3.path, 'E4.E3')
+
+    def test_path_stops_when_parent_has_no_name(self):
+        class P1(object):
+            class E2(Element):
+                class E3(Element):
+                    pass
+        self.assertEqual(P1.E2.E3.path, 'E2.E3')
+
+    def test_can_have_no_children(self):
+        self.assertEqual(Element.children, set())
+
+    def test_keeps_track_of_children(self):
+        class E(Element):
+            a = Element
+            b = Element
+            c = Element
+        self.assertEqual(E.children, set('a b c'.split()))
+
+    def test_child_keeps_track_of_children(self):
+        class E1(Element):
+            class E2(Element):
+                a = Element
+                b = Element
+                c = Element
+        self.assertEqual(E1.E2.children, set('a b c'.split()))
+
+    def test_inherits_children_from_superclass(self):
+        class E1(Element):
+            a = Element
+        class E2(E1):
+            b = Element
+        self.assertEqual(E2.children, set('a b'.split()))
+
+
+if __name__ == '__main__':
+    unittest.main()
